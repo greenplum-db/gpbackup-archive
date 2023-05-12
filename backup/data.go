d97ffef4554ca95b1c3a9a61466f7213a8d131e6
Andrew Repp <reppa@vmware.com>
2023-05-12 10:42:15 -0500

    Fix hang caused by piped copy commands failing
    A previous change to this code restructured how we track which tables
    are being backed up, and their status.  This led to the case where, if a
    piped copy command failed, the error was captured but never displayed.
    Instead we'd spin forever on an "Unknown" state in our sync.Map, causing
    permanent hangs.
    
    This commit restores the original behavior, where if a pipe fails the
    backup of the table is marked "Complete" in the sync.map, and the error
    is handled correctly downstream

--- backup/data.go
+++ backup/data.go
@@ -236,7 +236,8 @@ func backupDataForAllTables(tables []Table) []map[uint32]int64 {
 				err = BackupSingleTableData(table, rowsCopiedMaps[whichConn], &counters, whichConn)
 				if err != nil {
 					copyErr = err
-					break
+					oidMap.Store(table.Oid, Complete)
+					continue
 				} else {
 					oidMap.Store(table.Oid, Complete)
 				}
